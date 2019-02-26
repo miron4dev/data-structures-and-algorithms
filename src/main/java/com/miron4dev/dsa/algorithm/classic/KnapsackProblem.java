ad6468d9cb183d6bb79eb02742a04afe00512f2a
miron4dev <emironen0@gmail.com>
2019-02-26 18:55:24 +0200

    Knapsack Problem algorithm has been implemented

---
+++ src/main/java/com/miron4dev/dsa/algorithm/classic/KnapsackProblem.java
@@ -0,0 +1,35 @@
+package com.miron4dev.dsa.algorithm.classic;
+
+public class KnapsackProblem {
+
+    private final int numOfItems;
+    private final int capacity;
+    private final int[] weights;
+    private final int[] values;
+    private final int[][] knapsackTable;
+
+    public KnapsackProblem(int numOfItems, int capacity, int[] weights, int[] values) {
+        this.numOfItems = numOfItems;
+        this.capacity = capacity;
+        this.weights = weights;
+        this.values = values;
+        this.knapsackTable = new int[numOfItems + 1][capacity + 1];
+    }
+
+    public int solve() {
+        for (int i = 1; i <= numOfItems; i++) {
+            for (int w = 0; w <= capacity; w++) {
+
+                int notTakingItem = knapsackTable[i - 1][w];
+                int takingItem = 0;
+
+                if (weights[i - 1] <= w) {
+                    takingItem = values[i - 1] + knapsackTable[i - 1][w - weights[i - 1]];
+                }
+
+                knapsackTable[i][w] = Math.max(notTakingItem, takingItem);
+            }
+        }
+        return knapsackTable[numOfItems][capacity];
+    }
+}
---
+++ src/test/java/com/miron4dev/dsa/algorithm/classic/KnapsackProblemTest.java
@@ -0,0 +1,17 @@
+package com.miron4dev.dsa.algorithm.classic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class KnapsackProblemTest {
+
+    @Test
+    public void solve() {
+        int[] weights = {4, 2, 3};
+        int[] values = {10, 4, 7};
+
+        KnapsackProblem kp = new KnapsackProblem(3, 5, weights, values);
+        assertEquals(11, kp.solve());
+    }
+}
