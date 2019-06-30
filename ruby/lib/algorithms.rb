9fb06286a5525b0389069efffcf5dd0256e8baa4
miron4dev <miron4dev@gmail.com>
2019-06-30 15:05:12 +0300

    Ruby part has been removed

--- ruby/lib/algorithms.rb
+++
@@ -1,8 +0,0 @@
-module Algorithms
-  module Leetcode
-    module String end;
-  end
-end
-
-require 'algorithms/leetcode/string/anagram'
-require 'algorithms/leetcode/string/palindrome'
--- ruby/lib/version.rb
+++
@@ -1,3 +0,0 @@
-class Dsa
-  VERSION = '1.0'
-end
